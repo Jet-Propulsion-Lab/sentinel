d75a275b42b6a27dd5151f4387c9e50e404925a3
Hamza Bin Aamir <u2023219@giki.edu.pk>
2024-12-10 11:21:51 +0500

    Add interfacing for HAL sensors

--- Testing/Static/HAL.hpp
+++ Testing/Static/HAL.hpp
@@ -1,1 +1,29 @@
 //HAL
+
+#include <inttypes.h>
+#include <chrono>
+
+struct Force{
+    float Newtons;
+    float kGf;
+};
+
+class StrainSensor{
+public:
+    float Data; // Strain is unitless
+    std::chrono::milliseconds LastUpdateTime;
+    /// @brief DO NOT USE THIS AS AN IDENTIFIER, THIS IS USED TO SET THE INSTANCE ID, USE InstanceID INSTEAD
+    static int InstanceCount;
+    int InstanceID;
+};
+int StrainSensor::InstanceCount = 0;
+
+class ForceSensor{
+public:
+    Force Data;
+    std::chrono::milliseconds LastUpdateTime;
+    /// @brief DO NOT USE THIS AS AN IDENTIFIER, THIS IS USED TO SET THE INSTANCE ID, USE InstanceID INSTEAD
+    static int InstanceCount;
+    int InstanceID;
+};
+int ForceSensor::InstanceCount = 0;
