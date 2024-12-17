532fa2e6e03e24096639c82a088870e33e0cd0cd
SAreebZaheergit config --global user.name SAreebZaheer <sareebzaheer@gmail.com>
2024-12-17 19:54:29 +0500

    updated logger.h

--- logger.h
+++ logger.h
@@ -2,16 +2,67 @@
 #include <ostream>
 using namespace std;
 
+struct GPS
+{
+    float lat, lon;
+};
+
 struct log
 {
     string date;
     string time;
     int bearing;
-    float lat;
-    float lon;
+    GPS pos;   // GPS coordinates
+    float vel; // volcity
+    log *next, *prev;
+
+    log()
+    {
+        next = prev = nullptr;
+    }
 };
 
-void writelog(log data)
+class dlist
 {
+public:
+    log *head, *tail;
 
+    dlist()
+    {
+        head = tail = nullptr;
+    }
+
+    void insert(log data)
+    {
+        log *newNode = new log;
+        *newNode = data;
+
+        if (head == nullptr)
+        {
+            head = tail = newNode;
+        }
+        else
+        {
+            head->next = newNode;
+            newNode->prev = head;
+            head = newNode;
+        }
+    }
+};
+
+void readlogs(dlist logs)
+{
+    log *current = logs.tail;
+
+    while (current != nullptr)
+    {
+        cout << "<" << current->date << ">" << "<" << current->time << ">" 
+        << "<" << current->pos.lat << "," << current->pos.lon << ">" << "<" << current->bearing << ">" << "<" << current->vel << " m/s >" << endl;
+
+        current = current->next;
+    }
+}
+
+void writelog(dlist data)
+{
 }
