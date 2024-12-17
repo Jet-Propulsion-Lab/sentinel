6e25b80601559e353c182c2d3353f936777f649d
SAreebZaheergit config --global user.name SAreebZaheer <sareebzaheer@gmail.com>
2024-12-17 16:50:19 +0500

    updated abc.txt to doc.txt, added logger.h

---
+++ logger.h
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+struct log
+{
+    string date;
+    string time;
+    int bearing;
+    float lat;
+    float lon;
+};
+
+void writelog(log data)
+{
+
+}
